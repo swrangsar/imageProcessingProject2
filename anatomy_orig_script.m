5d427235992d1374d18a132a730e61dea81031c1
swrangsar <swrangsar@aol.com>
2012-10-25 16:11:11 +0530

    using column fft of matrix

--- anatomy_orig_script.m
+++ anatomy_orig_script.m
@@ -17,9 +17,8 @@ imageSize = size(inputImage);
 theta = 0:numberOfSpokes-1;
 theta = theta .* (180/numberOfSpokes);
 dataMatrix = radon(inputImage, theta);
-dataMatrix = fftshift(fft2(fftshift(dataMatrix)));
-
-imageMatrix = ifftshift(ifft2(ifftshift(dataMatrix)));
+dataMatrix = fft(dataMatrix, [], 1);
+imageMatrix = ifft(dataMatrix, [], 1);
 imageMatrix = iradon(imageMatrix, theta, 'linear', 'Ram-Lak', 1, imageSize(1));
 
 
